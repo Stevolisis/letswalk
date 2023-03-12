f6d213666adc3e573e4e5f63ba93b03cecebe6e5
Steven Joseph <stevolisisjoseph@gmail.com>
2023-03-12 19:36:39 +0100

    tested and trusted

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ app.use(bodyParser.json());
 
 
 const authorizedUsers=[
-    {regNo:'18/000155U/2',password:12345},
-    {regNo:'18/000255U/2',password:123456},
-    {regNo:'18/000355U/2',password:123457},
+    {regNo:'18/000155U/2',password:'12345'},
+    {regNo:'18/000255U/2',password:'123456'},
+    {regNo:'18/000355U/2',password:'123457'},
 ];
 
 
 
 app.get('/',(req,res)=>{
-    res.send('Api Working');
+    res.sendFile(__dirname+'/index.html');
 })
 
 
@@ -31,16 +31,20 @@ app.post('/login',(req,res)=>{
     const {regNo,password}=req.body;
     console.log(req.body)
 
-    if(regNo&&password){
-        authorizedUsers.forEach(user=>{
-            if(user.regNo===regNo&&user.password===password){
-                res.status(200).json({status:'success'})
-            }else{
-                res.status(401).json({status:'Invalid credentials'});
-            }
-        })
-
-    }else{
-        res.status(400).json({status:'error field not Found'})
+    try{
+        if(regNo!==''&&password!==''){
+            authorizedUsers.forEach(user=>{
+                if(user.regNo===regNo&&user.password===password){
+                    res.json({status:'success'})
+                }else{
+                    res.json({status:'Invalid credentials'});
+                }
+            })
+    
+        }else{
+            res.json({status:'error field not Found'})
+        }
+    }catch(err){
+        res.json({status:'error occured',message:err.message})
     }
 })
