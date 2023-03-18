fa619e2246aead6efc0f92638182a71020037f9e
Steven Joseph <stevolisisjoseph@gmail.com>
2023-03-18 22:58:14 +0100

    ignore case insensitive

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.listen(80,(res,err)=>{
         console.log("listening at port 80");
     }
 })
-
+//
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use(cors());
