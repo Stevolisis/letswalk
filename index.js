d0753da3874076d25d814cb6708bc423a6f226ff
Steven Joseph <stevolisisjoseph@gmail.com>
2023-03-31 19:56:29 +0100

    removed dotenv

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser=require('body-parser')
 const cors=require("cors");
 var admin = require("firebase-admin");
 var serviceAccount = require("./firebase.json");
-require('dotenv').config();
+// require('dotenv').config();
 
 app.listen(3000,(res,err)=>{
     if(err){
